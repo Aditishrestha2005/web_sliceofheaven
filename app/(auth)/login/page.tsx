f8e9254146a0fc69fe221aff97f551a10ba18be3
aditi <adishrestha2005@gmail.com>
2025-12-26 13:02:38 +0545

    folders and files created

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,31 @@
+// import LoginForm from "../_components/LoginForm";
+// import Image from "next/image";
+
+// export default function LoginPage() {
+//   return (
+//     <div className="min-h-screen bg-gradient-to-b from-orange-50 to-white flex items-center justify-center">
+//       <main className="flex items-center justify-center py-16 px-4 w-full">
+//         <div className="w-full max-w-md bg-white rounded-3xl shadow-xl p-10 text-center relative">
+
+//           {/* Decorative Circle behind image */}
+//           <div className="absolute -top-12 left-1/2 transform -translate-x-1/2 w-32 h-32 rounded-full bg-yellow-200 flex items-center justify-center shadow-md">
+//             <Image
+//               src="/pizza.jpg"
+//               alt="Pizza"
+//               width={96}
+//               height={96}
+//               className="rounded-full"
+//               priority
+//             />
+//           </div>
+
+//           <h1 className="mt-20 text-3xl font-extrabold text-orange-600 mb-6">
+//             Log In
+//           </h1>
+
+//           <LoginForm />
+//         </div>
+//       </main>
+//     </div>
+//   );
+// }
